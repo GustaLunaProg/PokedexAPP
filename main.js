2b657212775a5b26834cc6a4761d9ef68de0e586
Gustavo Luna <lunasgustavo@hotmail.com>
2022-08-29 23:10:31 -0300

    Pokedex APP

---
+++ main.js
@@ -0,0 +1,32 @@
+
+const getPokemon = () => {
+  const getBaseURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const pokemonPromises = [];
+
+  for (let i = 1; i <= 150; i++) {
+    pokemonPromises.push(
+      fetch(getBaseURL(i)).then((response) => response.json())
+    );
+  }
+
+  Promise.all(pokemonPromises).then((pokemons) => {
+    const lisPokemons = pokemons.reduce((accumulator,pokemon) =>{
+       
+       const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+        accumulator += `
+        <li class= "card ${types[0]}">
+        <img class="card-image"  alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png"/>
+        <h2 class = "card-title">${pokemon.id} ${pokemon.name}</h2>
+        <p class="card-subtitle " id="${types[0]}">${types.join(' : ')}</p>
+        </li>
+        `
+        return accumulator
+    },'')
+
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = lisPokemons
+  });
+};
+
+getPokemon();
